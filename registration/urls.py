92d369784f67e6cb509ebe225e29639acc618b87
milvus <thommy.python.dev@gmail.com>
2022-12-26 20:02:42 +0300

    signin

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RegisterCreateView
+from .views import RegisterCreateView, SignInView
 
 
 urlpatterns = [
     path('signup/', RegisterCreateView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,18 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 class RegisterCreateView(CreateView):
     form_class = RegisterForm
-    success_url = reverse_lazy('index')
     template_name = 'registration/signup.html'
+    success_url = reverse_lazy('signin')
+
+
+class SignInView(LoginView):
+    form_class = LoginForm
+    template_name = 'registration/signin.html'
+    success_url = reverse_lazy('index')
